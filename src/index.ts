dcfe725d61c171232f68018644ba386db27da559
Max Zakharzhevskiy <max.za@digitalpersona.com>
2019-01-30 01:16:30 -0800

    Ini

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+class out {
+    static print(s: any) {
+        console.log(s);        
+    }
+}
+
+function main() {
+    let FOR_YEAR = '2019';
+    const msPerDay = 1000 * 60 * 60 * 24;
+
+    let d = +new Date(+FOR_YEAR - 1, 11, 31 - 6); // get prev week
+
+    // get prev week Monday
+    for (let i = 0; i < 7; i++) {
+        d += msPerDay;
+
+        let currD = new Date(d);
+        if (currD.getDay() === 1) {
+            out.print(currD);
+        }
+    }
+
+    // for (let i = 0; i < 365 + 6 + 6; i++) {
+    //     d += msPerDay;
+
+    //     let currD = new Date(d);
+    //     currD = currD.toLocaleDateString('en-US', {
+    //         year: '2-digit',
+    //         day: '2-digit',
+    //         month: '2-digit'
+    //     })
+
+    //     out.print(currD);
+    // }
+}
+
+main();
