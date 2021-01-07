5e96397f573345f133da0e4f31fe1269f2dffdd2
Max Zakharzhevskiy <maxzz@msn.com>
2021-01-06 17:45:34 -0800

    fix: add code review

--- src/index.ts
+++ src/index.ts
@@ -7,44 +7,15 @@ class out {
 }
 
 function getWeeks(forYear: number): string {
+    const DATE_FORMAT = { year: '2-digit', day: '2-digit', month: '2-digit' };
+    const SEPARATOR = `${'/'.repeat(78)}`;
     const ms1day = 1000 * 60 * 60 * 24;
     const ms4days = ms1day * 4; // i.e. Monday + the rest 4 working days
     const ms7days = ms1day * 7;
 
-    let d = +new Date(forYear - 1, 11, 31 - 6); // get prev week
-
-    // get first Monday
-    let firstMonday;
-    for (let i = 0; i < 7; i++) {
-        d += ms1day;
-
-        let currD = new Date(d);
-        if (currD.getDay() === 1/*Monday*/) {
-            firstMonday = +new Date(currD);
-            break;
-        }
-    }
-
-    function getMonthName(d: Date): string {
-        let s = d.toLocaleDateString('en-US', {month: 'long'});
-        return s;
-    }
-
-    function zeros(num: number, nTo: number, fillChar: string = ' ') {
-        let n = ''+num;
-        return fillChar.repeat(nTo - n.length <= 0 ? 0 : nTo - n.length) + n;
-    }
-
-
-    let options = {
-        year: '2-digit',
-        day: '2-digit',
-        month: '2-digit'
-    };
-    let LINE = `${'/'.repeat(78)}`;
-
+    let firstMonday: number = getFirstMonday();
+    let prevWeekD2Year: number;
     let currentMonth = -1;
-    let prevWeekD2Year;
 
     for (let i = 0; i < 52 + 2; i++) {
         let d1 = new Date(firstMonday);
@@ -58,12 +29,12 @@ function getWeeks(forYear: number): string {
 
             let theLastIsSameYear = i === 0 || (y2 === prevWeekD2Year && y1 === y2);
             if (theLastIsSameYear) {
-                out.print(`\n${LINE}\n${zeros(m + 1, 2, '0')} ${getMonthName(d2)}\n${LINE}`);
+                out.print(`\n${SEPARATOR}\n${zeros(m + 1, 2, '0')} ${getMonthName(d2)}\n${SEPARATOR}`);
             }
         }
 
-        let ds1 = d1.toLocaleDateString('en-US', options);
-        let ds2 = d2.toLocaleDateString('en-US', options);
+        let ds1 = d1.toLocaleDateString('en-US', DATE_FORMAT);
+        let ds2 = d2.toLocaleDateString('en-US', DATE_FORMAT);
         
         let s = `${ds1} - ${ds2}`.replace(/\//g, '.');
         out.print(s);
@@ -78,10 +49,35 @@ function getWeeks(forYear: number): string {
     }
 
     return out.buffer;
+
+    function getFirstMonday() {
+        let d = +new Date(forYear - 1, 11, 31 - 6); // get prev week
+
+        let firstMonday: number;
+        for (let i = 0; i < 7; i++) {
+            d += ms1day;
+    
+            let currD = new Date(d);
+            if (currD.getDay() === 1/*Monday*/) {
+                firstMonday = +new Date(currD);
+                break;
+            }
+        }
+        return firstMonday;
+    }
+
+    function getMonthName(d: Date): string {
+        return d.toLocaleDateString('en-US', { month: 'long' });
+    }
+
+    function zeros(num: number, nTo: number, fillChar: string = ' ') {
+        const n = ''+num;
+        return fillChar.repeat(nTo - n.length <= 0 ? 0 : nTo - n.length) + n;
+    }
 }
 
 function main() {
-    let FOR_YEAR = '2019';
+    let FOR_YEAR = '2021';
 
     let s = getWeeks(+FOR_YEAR);
     console.log(s);
