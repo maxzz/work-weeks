48e98f70c94c56b2a77bc715e82872c34e979f67
Max Zakharzhevskiy <maxzz@msn.com>
2022-01-17 15:21:55 -0800

    add: work-weeks version output during runtime

--- src/work-weeks.ts
+++ src/work-weeks.ts
@@ -1,11 +1,14 @@
 import { formatMonths, getWeeks } from '.';
+const config = require('../package.json');
 
 function main() {
     function showHelp() {
         console.log('work-weeks prints out work weeks for year.\nProvide a valid 4 digit number for year to print out.');
     }
-    
-        let year = Number(process.argv.slice(2)[0] || new Date().getFullYear());
+
+    console.log(`work-weeks ${config.version} version`);
+
+    let year = Number(process.argv.slice(2)[0] || new Date().getFullYear());
     if (isNaN(year)) {
         showHelp();
         return -1;
